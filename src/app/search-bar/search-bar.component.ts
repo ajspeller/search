efdba5f019e39cefd8b2a37f9d9901df45f64e89
AJ Speller <ajspeller@gmail.com>
2020-01-19 22:20:21 -0500

    Added event emitter

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-
+  term;
+  @Output() submitted = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFormSubmit(event: any) {
+    console.log(this.term);
+    event.preventDefault();
+    this.submitted.emit(this.term);
   }
 
 }
